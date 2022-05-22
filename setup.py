bf057259b281f575c2cc1f2d2657c17fa2605fd4
yurinogueira <yurinogueira@id.uff.br>
2022-05-22 10:59:20 -0300

    hotfix: adjust sound

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def long_description():
 setup(
     name="pplay-pygame",
     packages=find_packages(include=["pplay"]),
-    version="2.1.2",
+    version="2.1.3",
     description="A simple game abstraction framework to pygame",
     long_description=long_description(),
     python_requires=">=3.7,",
